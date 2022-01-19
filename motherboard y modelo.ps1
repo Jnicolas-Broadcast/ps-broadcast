ef23be5b7365fe288bf86495f3e381736d4133c1
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-18 23:00:01 -0300

    .

--- motherboard y modelo.ps1
+++ motherboard y modelo.ps1
@@ -1,9 +1,12 @@
+$ConfirmPreference = "None"
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
 #$host.ui.RawUI.WindowTitle = “Obtener hardware y modelo de motherboard”
 
 # Copiar a portapapeles la salida de la variable
 #$OutputVariable1 = (cmd /c "wmic baseboard get product, Manufacturer") | Out-String | clip
-
 <# ---------------------------------------------------------------
+    https://stackoverflow.com/questions/14071012/how-do-i-automatically-answer-yes-to-a-prompt-in-powershell
 #>
 
 new-item -force -path c:\ -name "modelo-mb" -itemtype file
