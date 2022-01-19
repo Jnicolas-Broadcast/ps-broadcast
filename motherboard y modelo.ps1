fb0a687c45db82f1ca45d1036bf5ef29896c1b9a
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-19 12:38:03 -0300

    .

--- motherboard y modelo.ps1
+++ motherboard y modelo.ps1
@@ -9,6 +9,6 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted
     https://stackoverflow.com/questions/14071012/how-do-i-automatically-answer-yes-to-a-prompt-in-powershell
 #>
 new-item -force -path c:\ -name "modelo-mb" -itemtype file
-Add-Content -Path "c:\modelo-mb.txt" -Value (cmd /c "wmic baseboard get product, Manufacturer")
-Add-Content -Path "c:\modelo-mb.txt" -Value (cmd /c "hostname")
-Add-Content -Path "c:\modelo-mb.txt" -Value (cmd /c "ECHO --------------------------------------------------------------------")
+Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "wmic baseboard get product, Manufacturer")
+Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "hostname")
+Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "ECHO --------------------------------------------------------------------")
