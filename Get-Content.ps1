380ce63532115a31e6d5858f9e1dfbc04c1ccbec
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-18 22:55:25 -0300

    .

---
+++ Get-Content.ps1
@@ -0,0 +1,3 @@
+$host.ui.RawUI.WindowTitle = "tail powershell"
+# equivalente a comando "tail -f" de linux
+Get-Content -Path "\\10.1.3.171\c`$\Program Files (x86)\Lansweeper\Service\errorlog.txt" -Tail 0 -Wait
