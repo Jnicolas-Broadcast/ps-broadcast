00be148255df25ccf54880230cbc3f952b6168fc
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-19 15:35:36 -0300

    .

--- motherboard y modelo.ps1
+++ motherboard y modelo.ps1
@@ -1,6 +1,6 @@
 $ConfirmPreference = "None"
 #Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-Set-ExecutionPolicy  -ExecutionPolicy Unrestricted -Scope LocalMachine 
+Set-ExecutionPolicy  -ExecutionPolicy Unrestricted <#-Scope LocalMachine#>
 
 #$host.ui.RawUI.WindowTitle = “Obtener hardware y modelo de motherboard”
 
@@ -9,13 +9,6 @@ Set-ExecutionPolicy  -ExecutionPolicy Unrestricted -Scope LocalMachine
 <# ---------------------------------------------------------------
     https://stackoverflow.com/questions/14071012/how-do-i-automatically-answer-yes-to-a-prompt-in-powershell
 #>
- 
-<#new-item -force -path c:\ -name "\\10.1.3.171\c$\modelo-mb.txt" -itemtype file
-Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "wmic baseboard get product, Manufacturer")
-Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "hostname")
-Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "ECHO --------------------------------------------------------------------")
-#>
-
 
 #new-item -force -path "\\10.1.3.171\c$\" -name "modelo-mb.txt" -itemtype file
 Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "wmic baseboard get product, Manufacturer")
