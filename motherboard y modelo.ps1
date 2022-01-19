525bb8dbfe94019c98d026644c6be05709af2a22
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-19 14:21:31 -0300

    .

--- motherboard y modelo.ps1
+++ motherboard y modelo.ps1
@@ -9,14 +9,15 @@ Set-ExecutionPolicy  -ExecutionPolicy Unrestricted -Scope LocalMachine
 <# ---------------------------------------------------------------
     https://stackoverflow.com/questions/14071012/how-do-i-automatically-answer-yes-to-a-prompt-in-powershell
 #>
-
-<# 
-new-item -force -path c:\ -name "\\10.1.3.171\c$\modelo-mb.txt" -itemtype file
+ 
+<#new-item -force -path c:\ -name "\\10.1.3.171\c$\modelo-mb.txt" -itemtype file
 Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "wmic baseboard get product, Manufacturer")
 Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "hostname")
 Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "ECHO --------------------------------------------------------------------")
 #>
-new-item -force -Path "\\10.1.3.171\c$\" -name "modelo-mb.txt" -itemtype file
+
+
+#new-item -force -path "\\10.1.3.171\c$\" -name "modelo-mb.txt" -itemtype file
 Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "wmic baseboard get product, Manufacturer")
 Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "hostname")
 Add-Content -Path "\\10.1.3.171\c$\modelo-mb.txt" -Value (cmd /c "ECHO --------------------------------------------------------------------")
