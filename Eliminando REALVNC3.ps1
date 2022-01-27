340aa586a8f32eba80f84655dcc8bacecb34516b
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-27 12:05:59 -0300

    .

--- Eliminando REALVNC3.ps1
+++ Eliminando REALVNC3.ps1
@@ -4,9 +4,6 @@ https://drive.google.com/file/d/1ipO-w-Rk_KXV3xJ5IimdHYdxQrtXN1hz/view?usp=shari
 link acortado:
 https://bit.ly/3H78Oh3
 
-otro link:
-https://doc-00-2c-docs.googleusercontent.com/docs/securesc/4n6fudedjl37onmsgqcbomutr33kltmj/kc389agsm2dqgp28uv8mk495e2tf4tho/1643290650000/14882183533323680891/14882183533323680891/1ipO-w-Rk_KXV3xJ5IimdHYdxQrtXN1hz?e=download&authuser=0&nonce=nqddvcvm4i152&user=14882183533323680891&hash=vsgfsh1tnsiekpjbo0ru6j5r28qi6ij4
-
 Nombre del .bat:
 "eliminar REALVNC.bat"
 extraccion:
@@ -14,5 +11,5 @@ https://www.softzone.es/windows/como-se-hace/descargar-archivos-powershell/
 #>
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://drive.google.com/file/d/1ipO-w-Rk_KXV3xJ5IimdHYdxQrtXN1hz/view?usp=sharing","C:\eliminar REALVNC.bat")
-cd C:\
+Set-Location C:\
 & '.\eliminar REALVNC.bat'
