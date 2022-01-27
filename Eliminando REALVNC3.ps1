d0c523dc64d0dbae75e1aada7e7dbe2bdd7b872e
Joaquin Cabral <jcabral@uepccba.org.ar>
2022-01-27 10:16:22 -0300

    .

---
+++ Eliminando REALVNC3.ps1
@@ -0,0 +1,11 @@
+<# Eliminar REALVNC solucion 3
+Link de descarga del .bat
+https://drive.google.com/file/d/1ipO-w-Rk_KXV3xJ5IimdHYdxQrtXN1hz/view?usp=sharing
+link acortado:
+https://bit.ly/3H78Oh3
+
+Nombre del .bat:
+"eliminar REALVNC.bat"
+#>
+$client = new-object System.Net.WebClient
+$client.DownloadFile("https://bit.ly/3H78Oh3","C:\eliminar REALVNC.bat")
